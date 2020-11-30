bcd70baf0a2575025d5f0718dae85e4b39b147c1
ahmedamer2 <63519817+ahmedamer2@users.noreply.github.com>
2020-11-30 13:36:50 -0700

    added JavaDoc comments

--- src/view/SeatView.java
+++ src/view/SeatView.java
@@ -3,21 +3,31 @@ package view;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.util.ArrayList;
-
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
 import model.Seat;
 import model.ShowTime;
 
+/**
+ * The SeatView class is a class extending JPanel used to present the seat view to the user.
+ * @author Ahmed Amer, Yassin Bayoumy, Bikramjeet
+ * @version 1.0
+ * @since November 29, 2020
+ */
 public class SeatView extends JPanel {
+    //JPanel components and member variables
     JButton[] seatButtons = new JButton[100];
     JButton cancelButton;
     JLabel[] labels = new JLabel[10];
     JLabel screenHere = new JLabel();
     ShowTime st;
 
+    /**
+     * The Constructor for the SeatView which takes in a ShowTime object needed to present the seatView for that specific show time. It will also call the 
+     * initComponents method used to initialize the JPanel components and add them appropriately
+     * @param st The ShowTime object used by the SeatView
+     */
     public SeatView(ShowTime st) {
         this.st = st;
         initComponents();
@@ -28,11 +38,16 @@ public class SeatView extends JPanel {
         }
     }
 
+    /**
+     * The initComponents method is used to initialize, format, resize and setup all the JPanel components.
+     */
     private void initComponents() {
+        //setup JPanel
         setPreferredSize(new Dimension(850, 480));
         setLayout(null);
         setBackground(new Color(255, 153, 204));
 
+        //Initialize and assign Components
         screenHere = new JLabel("SCREEN HERE");
         screenHere.setFont(new java.awt.Font("Comic Sans MS", 0, 14));
         screenHere.setSize(100, 50);
@@ -76,16 +91,31 @@ public class SeatView extends JPanel {
         setVisible(true);
     }
 
+    /**
+     * The addBackListener method takes in an ActionListener to be added to the backButton which will listen
+     * for the user to press the backButton.
+     * @param e The ActionListener that will be used to listen for the cancelButton to be pressed.
+     */
     public void addBackListener(java.awt.event.ActionListener e) {
         cancelButton.addActionListener(e);
     }
 
+    /**
+     * The addSeatListener method takes in an ArrayList of type ActionListener to be a added to the buttons representing each seat. The Listeners will 
+     * listen to the appropriate button and wait for the user to click on it.
+     * @param e The ArrayList of type ActionListener used to listen to the seats.
+     */
     public void addSeatListeners(ArrayList<java.awt.event.ActionListener> e) {
         for (int i = 0; i < e.size(); i++) {
             seatButtons[i].addActionListener(e.get(i));
         }
     }
 
+    /**
+     * The getter getSeat is used to return the Seat the user picked at a given index.
+     * @param index The index of the chosen seat
+     * @return An object of type Seat representing the seat that the user picked.
+     */
     public Seat getSeat(int index) {
         return st.getSeatList().get(index);
     }
